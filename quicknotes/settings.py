a75455871b17e61c2f7adc9203bc64da3608037e
j <j@js-iMac.local>
2025-11-07 21:00:47 -0800

    added whitenost and localhost

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8*#calu!*$q@8oq&9&2=uotj09k%qzk@@v3mf-*_85(*i@ry-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quicknotes-ifst.onrender.com']
+ALLOWED_HOSTS = ['quicknotes-ifst.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
